f38364549f2c9182e6ec5b35a0e4dfb4f5a0ccb5
Ayman Ashraf <mems92003@gmail.com>
2026-06-01 18:17:17 +0300

    Added MVC Structure

---
+++ app/Models/Job.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+class Job
+{
+    public static function all(){
+        return [
+            ['title' => 'Software Engineer','salary' => '$1000'],
+            ['title' => 'Graphic Deisgner','salary' => '$2000']
+        ];
+    }
+}
